14e92013abed4c4ab0e0a8b598e2c56be9b182d7
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-03-30 23:15:51 +0100

    started working on  header section

---
+++ signin.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="">
+    <title>Sign in</title>
+</head>
+<body>
+    <main>
+    <header>
+    <div class="center">
+    <h1>You must Sign In First</h1>
+    <h2>dont have an account?</h2>
+    <h3>No worries, Create one below.</h3>
+    </div>
+    </header>
+    </main>
+</body>
+</html>
