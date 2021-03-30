dc298e6f3a425a363d88f23d2132c02b0c512c36
JAMAL EDDINE ELIDRISSI Yassir <67668097+JAMALEEY@users.noreply.github.com>
2021-03-30 21:49:55 +0100

    add index.php style.css and logo

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOME</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <div class="headercontainer">
+        <div class="header">
+            <div class="headerlogo">
+                <img src="assets/images/logo.png" alt="logoyoucode">
+            </div>
+            <div class="headermenu">
+
+            </div>
+        </div>
+
+    </div>
+</body>
+
+</html>
