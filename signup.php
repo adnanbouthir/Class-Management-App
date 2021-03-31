22634968fa6a1d4e5697191f530181d4f5dee682
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-03-31 20:48:33 +0100

    approved and changed to php extension

--- signup.php
+++ signup.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="style/signup.css">
     <title>Sign Up</title>
 </head>
 
@@ -13,21 +13,29 @@
     <main>
 
         <div class="center">
+            <br>
             <h1>Sign Up</h1>
-            <p>create your account so that you can acess our platform</p>
+            <br>
+            <p>Create your account so that you can access our platform</p>
+         </div>
+            <div class="formcontainer"> 
+
+            
             <form action="./includes/traitement.php" method="post">
                 <input type="text" name="FName" placeholder="First Name" required>
-                <input type="text" name="lName" placeholder="last Name" required>
+                <input type="text" name="lName" placeholder="Last Name" required>
                 <input type="email" name="email" placeholder="Email Adress" required>
-                <label for="date">birth year</label>
+                <!-- <label for="date">birth year</label> -->
                 <input type="date" name="birthday" id="" required>
                 <input type="password" name="password" placeholder="Password" required>
-                <input type="password" name="rpassword" placeholder="Password confirmation" required>
-                <input type="reset" value="reset">
-                <input type="submit" value="submit" name="signup_submit">
+                <input type="password" name="rpassword" placeholder="Password Confirmation" required>
+<div class="subres"> 
+                <input type="reset" value="Reset">
+                <input type="submit" value="Register" name="signup_submit">
+</div>
             </form>
         </div>
-
+       
 
 </body>
 
