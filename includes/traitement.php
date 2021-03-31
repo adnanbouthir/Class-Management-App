fc0ca4bc5a9e4490af664fadb350115a4a793107
Diouani <73253036+Diouani@users.noreply.github.com>
2021-03-31 13:32:44 +0100

    added signin querry

--- includes/traitement.php
+++ includes/traitement.php
@@ -6,8 +6,8 @@ include_once('./database.php');
 
 if (isset($_POST['signup_submit'])) {
 
-    $Fname_signup = $_POST['Fname'];
-    $Lname_signup = $_POST['Lname'];
+    $Fname_signup = $_POST['FName'];
+    $Lname_signup = $_POST['lName'];
     $birthday_signup = $_POST['birthday'];
     $email_signup = $_POST['email'];
     $password_signup = $_POST['password'];
@@ -16,22 +16,41 @@ if (isset($_POST['signup_submit'])) {
 
     session_start();
 
-
-
-
-    $email_query = "SELECT * FROM users WHERE user_email ='$email_signup'";
+    $email_query = "SELECT * FROM `users` WHERE user_email ='$email_signup'";
     $email_resultat = mysqli_query($link, $email_query);
 } else if (mysqli_num_rows($email_resultat) > 0) {
-    $_SESSION['erroremail'] = "Sorry... email already taken";
+    $_SESSION['email_taken'] = "Sorry... email already taken";
     header("Location: signup.php");
 } else if ($password_signup != $rpassword_signup) {
     $_SESSION['pass_no_match'] = "Password doesn't match";
 } else {
-    $query_signup = "INSERT INTO `users`( `user_fname`, `user_lname`, `user_birthday`, `user_email`, `user_password`) VALUES ([$Fname_signup],[$Lname_signup],[$birthday_signup],[$email_signup,[$password_signup])";
+    $query_signup = "INSERT INTO `users`( `user_fname`, `user_lname`, `user_birthday`, `user_email`, `user_password`) VALUES ('$Fname_signup','$Lname_signup','$birthday_signup','$email_signup','$password_signup')";
 
-    $result_signup = mysqli_query($link, $query);
+    $result_signup = mysqli_query($link, $query_signup);
+    header("Location: signin.php");
+}
+
+
+//signin querry
 
 
 
-    header("Location: login.php");
+if (isset($_POST['signin_submit'])) {
+
+    $email_signin = $_POST['email_signin'];
+    $password_signin = $_POST['password_signin'];
+
+
+    $signin_querry = "SELECT FROM `users` WHERE `user_email`='$email_signin' AND `user_password`='$password_signin' ";
+    $signin_resultat = mysqli_query($link, $signin_querry);
+
+    if (mysqli_num_rows($signin_resultat) > 0) {
+        $row = mysqli_fetch_assoc($signin_resultat);
+
+        $_SESSION['username'] = $row['user_email'];
+
+        header("Location: index.php");
+    }else{
+        header("Location: signin.php?error=Sorry... username or password invalid");   //if isset
+    }
 }
--- signup.php
+++ signup.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,25 +8,27 @@
     <link rel="stylesheet" href="">
     <title>Sign Up</title>
 </head>
+
 <body>
     <main>
-    
-    <div class="center">
-        <h1>Sign Up</h1>
-        <p>create your account so that you can acess our platform</p>
-        <form action="/includes/traitement.php" method="post">
-        <input type="text" name="FName" placeholder="First Name" required>
-        <input type="text" name="lName" placeholder="last Name" required>
-        <input type="email" name="email" placeholder="Email Adress" required>
-        <label for="date">birth year</label>
-        <input type="date" name="birthday" id="" required>
-        <input type="password" name="password" placeholder="Password" required>
-        <input type="password" name="rpassword" placeholder="Password confirmation" required>
-        <input type="reset" value="reset" >
-        <input type="submit" value="submit" name="signup_submit">
-        </form>
-    </div>
 
-    
+        <div class="center">
+            <h1>Sign Up</h1>
+            <p>create your account so that you can acess our platform</p>
+            <form action="./includes/traitement.php" method="post">
+                <input type="text" name="FName" placeholder="First Name" required>
+                <input type="text" name="lName" placeholder="last Name" required>
+                <input type="email" name="email" placeholder="Email Adress" required>
+                <label for="date">birth year</label>
+                <input type="date" name="birthday" id="" required>
+                <input type="password" name="password" placeholder="Password" required>
+                <input type="password" name="rpassword" placeholder="Password confirmation" required>
+                <input type="reset" value="reset">
+                <input type="submit" value="submit" name="signup_submit">
+            </form>
+        </div>
+
+
 </body>
+
 </html>
