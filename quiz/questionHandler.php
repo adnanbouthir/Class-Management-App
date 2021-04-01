ec32d3900162e67eca2e2fff41ea72ba39c8484e
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-04-01 23:07:03 +0100

    working on question treatment

---
+++ quiz/questionHandler.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+require '../includes/database.php';
+
+if(isset($_POST['userRole'])) {
+    $userRole = $_POST['userRole'];
+}
+
+if($userRole > 1) {
+    header("Location: http://localhost/quizapp/quizhomepage.php");    
+    exit();
+}
+
+
+if(isset($_POST['submitQuestion'])) {
+    
+    $questionNumber = $_POST['questionNumber'];
+    $subject = mysqli_real_escape_string($con, $_POST['subject']);
+    $answer = mysqli_real_escape_string($con, $_POST['answer']);
+    $answer = str_replace(' ', '',$answer);
+    $answer = strip_tags($answer);
+    $answer = strtolower($answer);
+    $answer = md5($answer);
+    $question = mysqli_real_escape_string($con, $_POST['question']);    
+    $question = strip_tags($question);  
+    $imagePath = '';  
+
+    if(!empty($_FILES['fileToUpload']['name'])) {
+        $imagePath = '/quizapp/assets/src/images/'.$_FILES['fileToUpload']['name'];
+    
+        $fullPath = $_SERVER["DOCUMENT_ROOT"].$imagePath;
+        
+        
+        if(move_uploaded_file($_FILES['fileToUpload']['tmp_name'],  $fullPath)) {
+            
+            echo "success";
+        }
+        else{
+            echo "failed";
+            
+        }
+    }
