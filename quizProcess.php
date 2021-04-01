722016fe056bdd2a19e0caa0e9c304e524efce19
adnanbouthir <77015884+adnanbouthir@users.noreply.github.com>
2021-04-01 22:22:04 +0100

    working on backend quiz questions select

--- quizProcess.php
+++ quizProcess.php
@@ -1,55 +1,26 @@
-<?php 
-
-if($userRole > 1) {
-    header("Location: ./");
+<?php if(!isset($_GET['subject']) && !isset($_POST['subject'])) {
+    header("Location: ");    
     exit();
 }
 
-$subjectOptions = array(
-    "php" => "php",
-    "phpOop" => "phpOop",
-    "html" => "html",
-    "css" => "css",
-    "javascript" => "javascript",
-    "SASS" => "SASS",
-    "REACT" => "REACT",
-    "nodejs" => "nodejs"
-);
+if($userRole > 1) {
+    header("Location:");    
+    exit();
+}
+
+if(isset($_POST['subject'])) {
+    $subject = $_POST['subject'];   
+}
+
+$query = 
+    " SELECT * " .
+    " FROM `questions`" .
+    " WHERE `subject`='$subject'";
+
+$result = mysqli_query($con, $query);
+
+$totalQuestions = mysqli_num_rows($result);
+
+$questionNumber = (int)$totalQuestions + 1;
 
 ?>
-    
-       
-        <div class="container" style="margin-top: 20px;">
-            <form id="indexForm" method="POST" action="quiz.php" name="uploadSubjectForm">
-                <input type="hidden" name="userLoggedIn" value="<?php echo $userLoggedIn; ?>">
-                <div class="form-group row">
-                    <label for="subjects" class="col-sm-2 col-form-label">Choose subject to upload</label>
-                    <div class="col-sm-10">
-                        <select type="select" class="form-control" id="subject" name="subject">
-                            <?php
-                             foreach($subjectOptions as $key => $value)
-                             {
-                                ?>                                
-                                <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
-                                <?php
-                             }
-                            ?>
-                        </select>           
-                    </div>                    
-                </div>
-                        
-                <div class="form-group row">
-                    <div class="col-sm-10">
-                    <button type="submit" class="btn btn-primary" id="submitUploadSubject" name="submitUploadSubject">Proceed</button>                
-                    </div>
-                </div>            
-            </form>
-        </div>
-
-        <!-- Optional JavaScript -->
-        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
-    </html>
